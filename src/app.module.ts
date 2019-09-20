b881d082228b33674e0e79746a0d62b9eb56ae43
ashleynguci <trucanhftu2@gmail.com>
2019-09-20 22:08:24 +0300

    Create and connect NestJs to database using TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JobsModule } from './jobs/jobs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [JobsModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), JobsModule],
 })
 export class AppModule {}
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,11 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'jobmanagement',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
