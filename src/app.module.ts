22847eb28791a9697ab335287ec2207059a70e01
janicetimelessbeauty <nga08102004@gmail.com>
2023-09-18 22:00:48 +1000

    Second

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,6 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), JobsModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfigannlrg), JobsModule, AuthModule],
 })
-export class AppModule {}
+export class AppModule {klsdkkl}
